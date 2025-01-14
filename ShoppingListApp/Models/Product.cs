3ae9286f87305dae7615339393034545e1b6470b
Kikol18 <155479656+Kikol18@users.noreply.github.com>
2025-01-14 12:38:27 +0100

    Add files via upload

---
+++ ShoppingListApp/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using ShoppingListApp.ViewModels;
+using System;
+
+namespace ShoppingListApp.Models
+{
+    [Serializable]
+    public class Product : INotifyPropertyChanged
+    {
+        private int quantity;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; } = string.Empty;
+        public int Quantity
+        {
+            get => quantity;
+            set
+            {
+                if (quantity != value)
+                {
+                    quantity = value;
+                    OnPropertyChanged(nameof(Quantity));
+                }
+            }
+        }
+        public string Unit { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+        public bool IsPurchased { get; set; }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
---
+++ ShoppingListApp/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingListApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
